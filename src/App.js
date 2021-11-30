aa183f249c33b514d9e75a17623e941edf8c7590
Sehyeon Park <nesmile23@gmail.com>
2021-11-30 15:17:09 +0900

    feat: Add to-do list layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import TodoCreate from "./components/TodoCreate";
+import TodoHeader from "./components/TodoHeader";
+import TodoList from "./components/TodoList";
+import { TodoProvider } from "./shared/TodoContext";
+import TodoTemplate from "./templates/TodoTemplate";
+
 function App() {
-  return <></>;
+  return (
+    <TodoProvider>
+      <TodoTemplate>
+        <TodoHeader />
+        <TodoCreate />
+        <TodoList />
+      </TodoTemplate>
+    </TodoProvider>
+  );
 }
 
 export default App;

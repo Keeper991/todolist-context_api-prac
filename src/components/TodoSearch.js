14d3acd16c545399a764959e243606a45702026e
Sehyeon Park <nesmile23@gmail.com>
2021-12-01 21:22:53 +0900

    feat: TodoSearch 추가

---
+++ src/components/TodoSearch.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef, useState } from "react";
+import { debounce } from "../shared/debounce";
+import { useTodoSetSearch } from "../shared/TodoContext";
+
+const TodoSearch = () => {
+  // useContext
+  const setSearch = useTodoSetSearch();
+
+  // hooks in local
+  const inputRef = useRef();
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    setSearch("");
+  }, [isOpen, setSearch]);
+
+  // handlers
+  const onToggle = () => setIsOpen(!isOpen);
+  const onChange = debounce((e) => {
+    setSearch(e.target.value);
+  }, 500);
+
+  return (
+    <div>
+      {isOpen && <input ref={inputRef} onChange={onChange} />}
+      <button onClick={onToggle}>{isOpen ? "x" : "🔍"}</button>
+    </div>
+  );
+};
+
+export default TodoSearch;

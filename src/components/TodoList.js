5e8dbae6b5070f054833aee023a5014f57626598
Sehyeon Park <nesmile23@gmail.com>
2021-12-03 13:59:02 +0900

    feat: sort에 따른 결과값이 list에 반영되도록 수정

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,48 @@
 import React from "react";
-import { useTodoSearch, useTodoState } from "../shared/TodoContext";
+import {
+  useTodoSearch,
+  useTodoSort,
+  useTodoState,
+} from "../shared/TodoContext";
 import TodoItem from "./TodoItem";
+
+const getSortFn = (sort) => {
+  switch (sort) {
+    case "createdAt":
+      return (a, b) => b.createdAt - a.createdAt;
+    case "description":
+      return (a, b) =>
+        a.description.toUpperCase() > b.description.toUpperCase() ? 1 : -1;
+    default:
+      throw new Error(`Unexpected sort type: ${sort}`);
+  }
+};
+
+const sortByIsDone = (a, b) => {
+  if (!a.isDone && b.isDone) return -1;
+  if (a.isDone && !b.isDone) return 1;
+  return 0;
+};
 
 const TodoList = () => {
   // contexts
   const todos = useTodoState();
   const search = useTodoSearch();
+  const sort = useTodoSort();
 
   const cases = [search.toLowerCase(), search.toUpperCase()];
 
   return (
     <div>
-      {todos.map(
-        (todo) =>
-          cases.some((c) => todo.description.includes(c)) && (
-            <TodoItem key={todo.id} {...todo} />
-          )
-      )}
+      {todos
+        .sort(getSortFn(sort))
+        .sort(sortByIsDone)
+        .map(
+          (todo) =>
+            cases.some((c) => todo.description.includes(c)) && (
+              <TodoItem key={todo.id} {...todo} />
+            )
+        )}
     </div>
   );
 };
